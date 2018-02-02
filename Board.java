0174c91f61db1dced403ff1b6924dea8f441317c
kaynen.mitchell1 <kaynen.mitchell1@cs.ucalgary.ca>
2018-02-02 14:37:32 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,4 +1,24 @@
 public class Board 
 	{
-	
+	    private int xLength;
+	    private int yLength;
+	    private int board;
+	    public Board(int x, int y)
+	        {
+	            this.xLength = x;
+	            this.yLength = y;
+	        }
+	    public makeBoard()
+	        {
+	            this.board = new int[6][5];
+	        }
+	    public Point getBoardSize()
+	        {
+	            Point size = new Point(this.xLength,this.yLength)
+	            return size
+	        }
+	    public static void main(String[] args) 
+	        {
+	            
+	        }
 	}
