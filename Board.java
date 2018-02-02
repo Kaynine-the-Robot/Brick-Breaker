1e20043f7cbebddb03bc17e658e3c433f1cc9c16
kaynen.mitchell1 <kaynen.mitchell1@cs.ucalgary.ca>
2018-02-02 14:46:35 -0700

    1st Change Board

--- Board.java
+++ Board.java
@@ -1,24 +1,21 @@
+import java.awt.Point;
 public class Board 
 	{
 	    private int xLength;
 	    private int yLength;
-	    private int board;
+	    private int[][] board;
 	    public Board(int x, int y)
 	        {
 	            this.xLength = x;
 	            this.yLength = y;
 	        }
-	    public makeBoard()
+	    public void makeBoard()
 	        {
-	            this.board = new int[6][5];
+	            this.board = new int[this.yLength][this.xLength];
 	        }
 	    public Point getBoardSize()
 	        {
-	            Point size = new Point(this.xLength,this.yLength)
-	            return size
-	        }
-	    public static void main(String[] args) 
-	        {
-	            
+	            Point size = new Point(this.xLength,this.yLength);
+	            return size;
 	        }
 	}
