f408643f581bf83e82600c588eb237800f968edb
sebastien.wong1 <sebastien.wong1@cs.ucalgary.ca>
2018-02-02 14:45:04 -0700

    Added methods and instance variables to Ball.java

--- Ball.java
+++ Ball.java
@@ -1,3 +1,5 @@
+import java.awt.Point;
+
 /**
  * This is the ball class which will have all the methods and attributes to control the ball 
  * and it's interactions.
@@ -6,5 +8,58 @@
  */
 public class Ball 
 	{
+	    
+	    private Point position;
+	    private int horzMovement = 1;
+	    private int vertMovement = 1;
+	    
+	    public Ball(int x, int y)
+	    {
+	        this.position = new Point(x, y);
+	    }
+	    
+	    /**
+	     * This method returns the position of the ball object.
+	     * @param args unused.
+	     * @return Ball position as a Point object.
+	     * 
+	     */
+	    public Point getPosition()
+	    {
+	        return position;
+	    }
+	    
+	    /**
+	     * This method updates the position of the ball object.
+	     * @param args unused.
+	     * @return Nothing.
+	     * 
+	     */
+	    public void updatePos()
+	    {
+	        position.move(horzMovement, vertMovement);
+	    }
+	    
+	    /**
+	     * This method changes the horizontal speed of the ball object when there has been a collision.
+	     * @param args unused.
+	     * @return Nothing.
+	     * 
+	     */
+	    public void horzCollision()
+	    {
+	        horzMovement = horzMovement * -1;
+	    }
+	    
+	    /**
+	     * This method changes the vertical speed of the ball object when there has been a collision.
+	     * @param args unused.
+	     * @return Nothing.
+	     * 
+	     */
+	    public void vertCollision()
+	    {
+	        vertMovement = vertMovement * -1;
+	    }
 	
 	}
