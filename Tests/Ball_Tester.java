6fe5e9b728df031436e22a4320910eebd34c22e7
Sebastien Wong <sebastien.wong1@ucalgary.ca>
2018-03-13 16:47:39 -0600

    Forgot to remove gui from main, so did that.

---
+++ Tests/Ball_Tester.java
@@ -0,0 +1,23 @@
+package Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Ball_Tester {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void test_Movement() {
+		
+		Ball b = new Ball(1, 1);
+		
+		
+		
+	}
+
+}
